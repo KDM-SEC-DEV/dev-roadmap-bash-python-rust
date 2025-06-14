ad9a7bc96d765e8027faa3b315ae984df9438365
KDM-SEC-DEV <kdmsecdev@email.com>
2025-06-14 14:42:31 -0500

    Add phase-1 Bash, Python, and Rust starter scripts

---
+++ phase-1-foundations/rust/target/debug/deps/rust.d
@@ -0,0 +1,5 @@
+C:\Users\ash2v\dev-roadmap-bash-python-rust\phase-1-foundations\rust\target\debug\deps\rust.exe: src\main.rs
+
+C:\Users\ash2v\dev-roadmap-bash-python-rust\phase-1-foundations\rust\target\debug\deps\rust.d: src\main.rs
+
+src\main.rs:
---
+++ phase-1-foundations/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+C:\Users\ash2v\dev-roadmap-bash-python-rust\phase-1-foundations\rust\target\debug\rust.exe: C:\Users\ash2v\dev-roadmap-bash-python-rust\phase-1-foundations\rust\src\main.rs
